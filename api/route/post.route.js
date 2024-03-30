62661c835dabb8084d40b1aef1a3d14db12e3b82
Hassan <hssnkhatri2@gmail.com>
2024-03-30 15:40:55 +0500

    complete create post functionality

--- api/route/post.route.js
+++ api/route/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { verifyUser } from '../utils/VerifyUser';
-import { create } from '../controller/post.controller';
+import { verifyUser } from '../utils/VerifyUser.js';
+import { create } from '../controller/post.controller.js';
 
 
 const router = express.Router();
