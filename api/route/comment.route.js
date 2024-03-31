a5fe03d84a094f480d47176d1582624e3b31dcf1
Hassan <hssnkhatri2@gmail.com>
2024-03-31 17:01:07 +0500

    recent post added to post page and comments also added to the dashboard

--- api/route/comment.route.js
+++ api/route/comment.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyUser } from '../utils/VerifyUser.js';
-import { createComment, deleteComment, editComment, getPostComments, likeComment } from '../controller/comment.controller.js';
+import { createComment, deleteComment, editComment, getPostComments, getcomments, likeComment } from '../controller/comment.controller.js';
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyUser, likeComment);
 router.put('/editComment/:commentId', verifyUser, editComment);
 router.delete('/deleteComment/:commentId', verifyUser, deleteComment);
-
+router.get('/getcomments', verifyUser, getcomments);
 
 
 export default router;
